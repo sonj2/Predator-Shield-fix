3f2139dfe7f2fab81d4e3ac7a48c2e521e5f4ac1
Andrew Son <sonj2@rpi.edu>
2020-03-14 13:35:16 -0400

    Attempt to fix zoom on non smg akimbos
    as well as for the akimbo mp5

--- Hooks/weaponfactorytweakdata.lua
+++ Hooks/weaponfactorytweakdata.lua
@@ -9,12 +9,21 @@ Hooks:PostHook(WeaponFactoryTweakData, "init", "WeaponFactoryTweakDatainit_akimb
 				local _npc_k = k .. "_npc"
 				if wFac[_npc_k] and v.uses_parts and wFac[_npc_k].uses_parts then
 					for _, v2 in pairs(_parts_name or {}) do
-						table.insert(wFac[k].uses_parts, v2)
-						table.insert(wFac[_npc_k].uses_parts, v2)					
+						table.insert(wFac[k].uses_parts, v2)					
+						table.insert(wFac[_npc_k].uses_parts, v2)
+					end
+					if k == "wpn_fps_smg_x_mp5" then
+						wFac[k].override = {mod_shield.stats.zoom = 2}
+					end
+					for type in wFac[k].categories then
+						if type == "pistol" then
+							wFac[k].override = {mod_shield.stats.zoom = 20}
+						end
 					end
 				end
 			end
 		end
 	end
+	wFac.parts.mod_shield.override_weapon_multiply = {CLIP_AMMO_MAX = 0.5}
 end)
 
